46c77f3b5d4aa53b4e1adae7c4ff1616cb5a7c72
Yevhen Sosna <yevhensosna@gmx.de>
2025-02-04 22:09:59 +0100

    remove old code

--- src/main/java/com/sosna/ollama/ocr/config/MultipartResolver.java
+++
@@ -1,9 +0,0 @@
-package com.sosna.ollama.ocr.config;
-
-@Bean
-public MultipartResolver multipartResolver() {
-    CommonsMultipartResolver multipartResolver
-      = new CommonsMultipartResolver();
-    multipartResolver.setMaxUploadSize(5242880);
-    return multipartResolver;
-}
